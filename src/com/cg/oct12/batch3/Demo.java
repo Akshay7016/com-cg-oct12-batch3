4df733699a52ba63a7aed0c6e3db91bff6866f86
Akshay Sajgule <asajgule@gmail.com>
2021-12-14 23:20:51 +0530

    Commit

--- src/com/cg/oct12/batch3/Demo.java
+++ src/com/cg/oct12/batch3/Demo.java
@@ -1,10 +1,29 @@
 package com.cg.oct12.batch3;
 
-public class Demo {
 
+public class Demo {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+		
 
 	}
+	
+//	public User login(String userName, String password) {
+//		if(!usRepository.finbdByUserName(userName).isEmpty()) {
+//			User user = usRepository.finbdByUserName(userName);
+//			
+//			if(usRepository.finbdByPassword(user.getPassword()).equals(password)) {
+//				Log.info("Sucess");
+//			}
+//			else {
+//				throw new CustomException("hgfjhadgjhagakha");
+//			}
+//		}
+//		else {
+//			throw new CustomException("hgfjhadgjhagakha");
+//		}
+//	}
 
 }
